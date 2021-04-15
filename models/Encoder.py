0018833a1d70cf7faf6b6e9e9d2207f429d037b1
Zichun Hao <zichunh2000@gmail.com>
2021-04-15 00:33:16 -0700

    Fix typos

--- models/Encoder.py
+++ models/Encoder.py
@@ -6,7 +6,7 @@ from models.GraphNet import GraphNet
 
 class Encoder(nn.Module):
 
-    def __init__(self, num_node, node_sizes, output_node_size, nums_hidden_node_layers, hidden_edge_sizes, output_edge_sizes,
+    def __init__(self, num_nodes, node_sizes, output_node_size, nums_hidden_node_layers, hidden_edge_sizes, output_edge_sizes,
                  nums_mps, dropout, alpha, intensities, batch_norm=True):
         super(Encoder, self).__init__()
 
@@ -19,7 +19,7 @@ class Encoder(nn.Module):
         for var_list in [nums_hidden_node_layers, hidden_edge_sizes, output_edge_sizes, nums_mps, intensities]:
             expand_var_list(var_list, len(node_sizes))
 
-        self.num_node = num_node
+        self.num_nodes = num_nodes
         self.node_sizes = node_sizes
         self.output_node_size = output_node_size
         self.nums_hidden_node_layers = nums_hidden_node_layers
@@ -33,13 +33,13 @@ class Encoder(nn.Module):
 
         self.encode = nn.ModuleList()
         for i in range(self.num_layers-1):
-            graphNet = GraphNet(num_nodes=self.num_node, input_node_size=self.node_sizes[i], output_node_size=self.node_sizes[i+1],
+            graphNet = GraphNet(num_nodess=self.num_nodes, input_node_size=self.node_sizes[i], output_node_size=self.node_sizes[i+1],
                                 num_hidden_node_layers=nums_hidden_node_layers[i], hidden_edge_size=hidden_edge_sizes[i],
                                 output_edge_size=output_edge_sizes[i], num_mp=nums_mps[i], dropout=dropout, alpha=alpha,
                                 intensity=self.intensities, batch_norm=batch_norm)
             self.encode.append(graphNet)
 
-        graphNet = GraphNet(num_nodes=self.num_node, input_node_size=self.node_sizes[-1], output_node_size=self.output_node_size,
+        graphNet = GraphNet(num_nodess=self.num_nodes, input_node_size=self.node_sizes[-1], output_node_size=self.output_node_size,
                             num_hidden_node_layers=nums_hidden_node_layers[-1], hidden_edge_size=hidden_edge_sizes[-1],
                             output_edge_size=output_edge_sizes[-1], num_mp=nums_mps[-1], dropout=dropout, alpha=alpha,
                             intensity=self.intensities[-1], batch_norm=batch_norm)
