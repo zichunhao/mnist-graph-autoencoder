9ded6b3d523c9143d0d20cc86979afa96b80ee19
Zichun Hao <zichunh2000@gmail.com>
2021-04-24 14:26:40 -0700

    Remove unnecessary dependencies on args

--- utils.py
+++ utils.py
@@ -14,8 +14,8 @@ def generate_img_arr(coords, img_dim=28):
     coords = coords.detach().numpy()
     coords = np.array(coords, copy=True)
     # Denormalization
-    coords[:, :2]  = (coords[:, :2] + 1) / 2 * img_dim - 1e-5
-    coords[:, -1] = coords[:, -1] * 255 + 127.5
+    coords[:, :2]  = (coords[:, :2] + 1) / 2 * img_dim - 1e-5  # x,y
+    coords[:, -1] = (coords[:, -1] + 1) * 127.5  # Intesity
 
     img_arr = np.zeros((img_dim, img_dim))
     for pts in coords:
@@ -50,7 +50,7 @@ def generate_id():
 '''
 Save generated images.
 '''
-def save_gen_imgs(args, gen_imgs, labels, epoch, is_train, outpath):
+def save_gen_imgs(gen_imgs, labels, epoch, is_train, outpath):
     make_dir(f"{outpath}/generated_images/train")
     make_dir(f"{outpath}/generated_images/valid")
     for i in range(len(gen_imgs)):
@@ -64,7 +64,7 @@ def save_gen_imgs(args, gen_imgs, labels, epoch, is_train, outpath):
 '''
 Save data like losses and dts.
 '''
-def save_data(args, data, data_name, epoch, is_train, outpath, global_data=False):
+def save_data(data, data_name, epoch, is_train, outpath, global_data=False):
     make_dir(f"{outpath}/model_evaluations/pkl_files")
     if not global_data:
         if is_train:
