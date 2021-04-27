31708f59bcb4f3ab30004e6980fa6523203b4bb1
Zichun Hao <zichunh2000@gmail.com>
2021-04-26 22:03:34 -0700

    Resolve errors

--- utils/loss.py
+++ utils/loss.py
@@ -6,33 +6,11 @@ class ChamferLoss(nn.Module):
     def __init__(self):
         super(ChamferLoss, self).__init__()
 
+    # Adapted from Steven Tsan https://github.com/stsan9/AnomalyDetection4Jets/blob/emd/code/loss_util.py#L3
     def forward(self, x, y):
         num_pts = x.shape[0]
-        dist[:] = torch.pow(torch.cdist(x,y),2)
-        in_dist_out = torch.min(dist, dim=0)
-        out_dist_in = torch.min(dist, dim=1)
+        dist = torch.pow(torch.cdist(x,y),2)
+        in_dist_out = torch.min(dist, dim=1)
+        out_dist_in = torch.min(dist, dim=2)
         loss = torch.sum(in_dist_out.values + out_dist_in.values) / num_pts
-        loss.requres_grad = True
-
-'''Unused in this project'''
-# from Steven Tsan https://github.com/stsan9/AnomalyDetection4Jets/blob/emd/code/loss_util.py#L3
-def chamfer_loss(x, y):
-    nparts = x.shape[0]
-    dist = torch.pow(torch.cdist(x,y),2)
-    in_dist_out = torch.min(dist,dim=0)
-    out_dist_in = torch.min(dist,dim=1)
-    loss = torch.sum(in_dist_out.values + out_dist_in.values) / nparts
-    loss.requres_grad = True
-    return loss
-
-# Reconstruction + KL divergence losses
-def vae_loss(x, y, mu, logvar):
-    BCE = chamfer_loss(x,y)
-
-    # see Appendix B from VAE paper:
-    # Kingma and Welling. Auto-Encoding Variational Bayes. ICLR, 2014
-    # https://arxiv.org/abs/1312.6114
-    # 0.5 * sum(1 + log(sigma^2) - mu^2 - sigma^2)
-    KLD = -0.5 * torch.sum(1 + logvar - mu.pow(2) - logvar.exp())
-
-    return BCE + KLD
+        return loss
