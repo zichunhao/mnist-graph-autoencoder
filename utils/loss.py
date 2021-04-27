a15ea5f4263e4239c9e2e302867dd19b60d843c2
Zichun Hao <zichunh2000@gmail.com>
2021-04-26 23:07:55 -0700

    Add epsilon to avoid gradient divergence

--- utils/loss.py
+++ utils/loss.py
@@ -9,7 +9,7 @@ class ChamferLoss(nn.Module):
     # Adapted from Steven Tsan https://github.com/stsan9/AnomalyDetection4Jets/blob/emd/code/loss_util.py#L3
     def forward(self, x, y):
         num_pts = x.shape[0]
-        dist = torch.pow(torch.cdist(x,y),2)
+        dist = torch.pow(torch.cdist(x,y),2) + 1e-6
         in_dist_out = torch.min(dist, dim=1)
         out_dist_in = torch.min(dist, dim=2)
         loss = torch.sum(in_dist_out.values + out_dist_in.values) / num_pts
