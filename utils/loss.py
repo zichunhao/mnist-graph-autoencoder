6e30afd9a177dd0e3dfe7fdc7ba557dd3978c2aa
Zichun Hao <zichunh2000@gmail.com>
2021-06-23 15:13:40 -0700

    Update references

--- utils/loss.py
+++ utils/loss.py
@@ -14,14 +14,19 @@ class ChamferLoss(nn.Module):
         min_dist_xy = torch.min(dist, dim=-1)
         min_dist_yx = torch.min(dist, dim=-2)  # Equivalent to permute the last two axis
 
-        # Adapted from Steven Tsan https://github.com/stsan9/AnomalyDetection4Jets/blob/emd/code/loss_util.py#L3
+        # Adapted from Steven Tsan https://github.com/stsan9/AnomalyDetection4Jets/blob/b31a9a2af927a79093079911070a45f14a833c14/code/loss_util.py#L26
         loss = torch.sum(min_dist_xy.values + min_dist_yx.values)
 
         return loss
 
-def pairwise_distance(x, y, device):
-    assert (x.shape[0] == y.shape[0]), f"The batch size of x and y are not equal! x.shape[0] is {x.shape[0]}, whereas y.shape[0] is {y.shape[0]}!"
-    assert (x.shape[-1] == y.shape[-1]), f"Feature dimension of x and y are not equal! x.shape[-1] is {x.shape[-1]}, whereas y.shape[-1] is {y.shape[-1]}!"
+def pairwise_distance(x, y, eps=1e-12, device=None):
+    if (x.shape[0] != y.shape[0]):
+        raise ValueError(f"The batch size of x and y are not equal! x.shape[0] is {x.shape[0]}, whereas y.shape[0] is {y.shape[0]}!")
+    if (x.shape[-1] != y.shape[-1]):
+        raise ValueError(f"Feature dimension of x and y are not equal! x.shape[-1] is {x.shape[-1]}, whereas y.shape[-1] is {y.shape[-1]}!")
+
+    if device is None:
+        device = x.device
 
     batch_size = x.shape[0]
     num_row = x.shape[1]
@@ -31,6 +36,6 @@ def pairwise_distance(x, y, device):
     x1 = x.repeat(1, 1, num_col).view(batch_size, -1, num_col, vec_dim).to(device)
     y1 = y.repeat(1, num_row, 1).view(batch_size, num_row, -1, vec_dim).to(device)
 
-    dist = torch.norm(x1 - y1 + 1e-12, dim=-1)
+    dist = torch.norm(x1 - y1 + eps, dim=-1)
 
     return dist
